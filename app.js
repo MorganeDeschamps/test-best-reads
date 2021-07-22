7e443e406cd9b3de8e17749946c1326beaba4457
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-07-22 12:25:47 +0200

    specified rott path for sendFile

--- app.js
+++ app.js
@@ -10,11 +10,10 @@ require("./config")(app);
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     // If no routes match, send them the React HTML.
     res.sendFile(__dirname + "/public/index.html");
   });
-
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
